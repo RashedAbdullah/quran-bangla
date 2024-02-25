137092a904a65450b6abb09a850000d65c0fd71d
Rashed Abdullah <maarashed40@gmail.com>
2024-02-25 09:32:39 +0600

    loading animation

--- src/layouts/layout.jsx
+++ src/layouts/layout.jsx
@@ -4,6 +4,7 @@ import { Outlet, ScrollRestoration, useNavigation } from "react-router-dom";
 import { ThemeContext } from "../contexts/themeProvider";
 import { useContext } from "react";
 import { motion } from "framer-motion";
+import { Box, Skeleton } from "@mui/material";
 
 const Layout = () => {
   const navigation = useNavigation();
@@ -15,7 +16,17 @@ const Layout = () => {
     <div className="bg-secondary font-Siliguri selection:first-letter:bg-green-600 selection:bg-secondary selection:text-textWhite">
       <Header />
       <ScrollRestoration />
-      {navigation.state === "loading" ? <div>Loading Data</div> : <Outlet />}
+      {navigation.state === "loading" ? (
+        <div className="flex h-screen justify-center align-middle">
+          <Box sx={{ width: 300 }}>
+            <Skeleton />
+            <Skeleton animation="wave" />
+            <Skeleton animation={false} />
+          </Box>
+        </div>
+      ) : (
+        <Outlet />
+      )}
       <Footer />
     </div>
   );
